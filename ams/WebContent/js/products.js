8e13355f1c65e242e10b186151bb47c831cff2a6
gspleesters <guys@e-mon.eu>
2015-05-05 23:12:05 +0200

    car 3

--- ams/WebContent/js/products.js
+++ ams/WebContent/js/products.js
@@ -3,11 +3,16 @@
  */
 $(document).ready(function ()
 {
+	/*
 	$('#Map').mapster({
 	singleSelect : true,
 	clickNavigate : true,
      mapKey: 'color',
 	fill : true,
 	fillOpacity : 1,
-});
+	});
+	*/
+	
+	$('#map_id').on('click', function () {alert("AREA CLICKED" );   });
+	
 });
